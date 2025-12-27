9f33fab7da01ff528981fd9969e10123ad7e38ec
Elmer <bantoyelmer2@gmail.com>
2025-12-28 01:26:55 +0800

    my first commit

---
+++ practiceConsole/InfoStudent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceConsole
+{
+   class InfoStudent
+    {
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+
+            set
+            {
+                if (value < 0)
+                  Console.WriteLine("Age cannot be negative");
+                    _age = value;
+               
+             
+            }
+
+            
+        }
+    
+    }
+}
---
+++ practiceConsole/StudentService.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using practiceConsole;
+
+class StudentService
+{
+    public string getStudentInfo(string name, int age)
+    {
+       return $"Name: {name}\n Age: {age}";
+    }
+
+    public string getStudentStatus(int status)
+    {
+        return status >= 18 ? "Adult" : "Minor";
+    }
+
+    public double studentGrades(double prelim, double midterm, double final)
+    {
+      return (prelim + midterm + final) /3;
+    }
+
+    public string studentAverage(double average)
+    {
+        return average >= 75 ? "Passed" : "Failed";
+    }
+    static void Main(string[] args)
+    {
+        //StudentService student1 = new StudentService();
+        //StudentService student2 = new StudentService();
+
+        //string name1 = student1.getStudentInfo("Elmer", 19);
+        //string checkers = student1.getStudentStatus(19);
+        //double grades1 = student1.studentGrades(90, 98, 94);
+        //string average1 = student1.studentAverage(grades1); 
+        //Console.WriteLine($"{name1}\n {checkers} \n {grades1} {average1}");
+
+        InfoStudent stud = new InfoStudent();
+
+        stud.Age = -2;
+        int myAge = stud.Age;
+
+        Console.WriteLine(myAge);
+
+
+    }
+}
