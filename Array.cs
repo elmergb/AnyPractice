97f4dcc26efcf578a61af6a2c57910338de17075
elmergb <bantoyelmer2@gmail.com>
2026-01-05 21:10:13 +0800

    Add files via upload

---
+++ Array.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace pracArrayEnum
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            #region --one dimensional array
+            //Console.WriteLine("Hello, World");
+            Console.WriteLine("Origilnal: ");
+            string[] names = {"Elmer", "Heather","Ymir" };
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            names[1] = "Savanah";
+            foreach (string name in names)
+            {
+                Console.WriteLine("Modified: " + name);
+            }
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("Using for loop: " + names[i] );
+            }
+            #endregion
+
+            #region --multi dimensional array
+            //var cells = new int[3][]
+            //{
+            //   new [] {1,2,3 },
+            //   new[] { 4,5,6},
+            //   new [] {7,8,9 }
+            //};
+
+            //for (int row = 0; row < cells.Length; row++)
+            //{
+            //    Console.WriteLine("Row: " + row);
+
+            //    foreach (var i in cells[row])
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            #endregion
+
+            #region IEnumerable, Collection and List
+            Console.WriteLine("===========IEnumerable===========");
+            var city = new[] {"Quezon City", "Bulacan", "Manila", "Cavite"};
+
+            IEnumerable<string> enumrableCity = city;
+
+            foreach (string i in city)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine(city[0]);
+            Console.WriteLine("===========Collection===========");
+            ICollection<string> colCountries = new List<string>(city);
+            colCountries.Add("Laguna");
+            colCountries.Remove("Manila");
+            colCountries.Remove(city[0]);
+
+            foreach (string i in colCountries)
+            {
+                Console.WriteLine(i); 
+            }
+
+            Console.WriteLine("===========List===========");
+
+
+            #endregion
+        }
+    }
+}
---
+++ List.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace SimpleSystem
+{
+
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+    }
+
+    class Countries
+    {
+        public string CountiresName { get; set; }
+    }
+    class StudentCrud
+    {
+        static void Main(string[] args)
+        {
+            #region simple List practice 
+            //Boolean isRunning = true;
+            //List<Student> students = new List<Student>();
+            //while (isRunning) { 
+            //Console.Write("Enter your name: ");
+            //string name = Console.ReadLine();
+                
+            //    if (name.ToLower() == "1")
+            //    {
+            //        foreach (var student in students)
+            //        {
+            //            Console.WriteLine($"Name: {student.Name}, Age: {student.Age}");
+            //        }
+            //    }
+            //Console.Write("Enter your age: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //students.Add(new Student
+            //{
+            //    Name = name,
+            //    Age = age
+            //});
+             
+               
+            //}
+            #endregion
+
+            var countries = new[] {"Philippines", "USA", "Japan", "Singapore" };
+            
+            IEnumerable<string> nameCount = countries;
+            IList<Countries> country = new List<Countries>();
+            IList<Student> students = new List<Student>();
+            Boolean isRunning = true;   
+
+            Console.WriteLine("===========Book your flight============");
+            Console.WriteLine("1. Show all Country");
+            Console.WriteLine("2. Show all student their flight");
+            Console.WriteLine("3. Add Student flight");
+            Console.WriteLine("4. Exit");
+            Console.Write("Select: ");
+            int select = Convert.ToInt32(Console.ReadLine());
+
+            switch (select)
+            {
+                case 1:
+                    //int count = 1;
+
+                    //foreach (string country in countries)
+                    //{
+                    //    Console.WriteLine($"{count} : {country}");
+                    //    count++;
+                    //}
+
+                    for (int i = 0; i < countries.Length; i++)
+                    {
+                        Console.WriteLine($"{i + 1} : {countries[i]}" );
+                    }
+                    break;
+                case 2:
+
+                    break;
+
+            }
+        }
+    }
+}
