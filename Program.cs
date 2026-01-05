8318ee835f39079dfd1d115ec03ac86b107e3205
elmergb <bantoyelmer2@gmail.com>
2026-01-05 21:30:48 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Linq;
+
+namespace LinqLamda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var countries = new[] {"Philippines", "USA", "Japan", "Singapore" };
+
+            var linqResult = from country in countries where country.Contains("P")  select country;
+            foreach (string country in linqResult)
+            {
+                Console.WriteLine(country);
+            } 
+
+        }
+    }
+}
