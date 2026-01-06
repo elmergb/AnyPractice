a22be9def079dd7ea6a70fc6d55f8dad64701f81
elmergb <bantoyelmer2@gmail.com>
2026-01-06 22:29:10 +0800

    Add files via upload
    Practice Linq and Lamda

---
+++ LinqLamda.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Linq;
+
+namespace LinqLamda
+{
+    internal class Program
+    {
+        class Item
+        {
+            public int item { get; set; }
+            public string price { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            #region practice Linq
+            //    var countries = new[] {"Philippines", "USA", "Japan", "Singapore" };
+            //    var expense = new [] { 100, 200, 300, 400, 500, 600 };
+
+            //    var linqResult = from country in countries where country.Contains("P")  select country;
+            //    foreach (string country in linqResult)
+            //    {
+            //        Console.WriteLine(country);
+            //    } 
+
+            //    var lingExpense = from exp in expense where exp >= 500 select  exp;
+            //    int count = 0;
+            //    foreach (var exp in lingExpense)
+            //    {
+            //      count++;
+            //      Console.WriteLine(count + "." + exp);
+            //    }
+
+            #endregion
+
+            ICollection<Item> item = new List<Item>();
+
+            Console.WriteLine("=========Sele");
+        }
+    }
+}
