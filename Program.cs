299fb09c2d254f2649d2d7986ee1ab48e71a9f9a
Elmer Bantoy <bantoyelmer2@gmail.com>
2026-01-15 22:12:03 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,62 @@
-﻿namespace Program
+﻿using System;
+
+public static class Hello
 {
-    internal class Program
+   
+    public static string Upper(this string s)
     {
-        public void printName (params string[] name)
+        if (string.IsNullOrEmpty(s))
         {
-            Console.WriteLine(string.Join(", ", name));
+            return s;
         }
 
-        public int sampleNumber(int num)
-        {
-            if (num == 1)
-                return num;
+        return char.ToUpper(s[0]) + s.Substring(s[1]);
+    }
 
-            return num * sampleNumber(num - 1);
+}
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        List<string> students = ["elmer", "heather", "savanah" ];
+
+        var num = String.Join(",", number); 
+
+        foreach (var student in students)
+        {
+            Console.WriteLine($@"Name: {student.Upper}");
         }
-        static void Main(string[] Args)
+
+        Console.WriteLine(num);
+        foreach (var r in num)
         {
-            var program = new Program();
+            Console.Write(r);
+        }
+        //string songLyrics = "You say goodbye, and I say hello";
+        //Console.WriteLine(songLyrics.Contains("goodbye"));
+        //Console.WriteLine(songLyrics.Contains("greetings"));
+        string sayHello = "Hello World!";
+        Console.WriteLine(sayHello);
+        sayHello = sayHello.Replace("World!", "Greetings");
+        Console.WriteLine(sayHello);
+        Console.WriteLine($"{number[2]}");
+        Console.WriteLine("----------");
+        string songLyrics = "You say goodbye, and I say hello";
+        Console.WriteLine(songLyrics.StartsWith("You"));
+        Console.WriteLine(songLyrics.StartsWith("goodbye"));
 
-            program.printName("Elmer", "Heather", "Savanah");
+        Console.WriteLine(songLyrics.EndsWith("hello"));
+        Console.WriteLine(songLyrics.EndsWith("goodbye"));
 
-            var sum = program.sampleNumber(3);
-
-            Console.WriteLine(sum);
+        if (songLyrics.StartsWith("Y"))
+        {
+            Console.WriteLine("Yes");
+        } else
+        {
+            Console.WriteLine("hINDI E");
         }
     }
+
+
 }
